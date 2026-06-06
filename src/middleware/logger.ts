cbf4387337b96a5e565c9545e0fa14822fdffe35
keiank <278810033+keiank@users.noreply.github.com>
2026-06-06 19:30:07 -0400

    Create logging middleware

---
+++ src/middleware/logger.ts
@@ -0,0 +1,8 @@
+import type { Request, Response, NextFunction } from 'express';
+
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log(`${req.method} - ${req.path}`);
+  next();
+}
+
+export { logger };
