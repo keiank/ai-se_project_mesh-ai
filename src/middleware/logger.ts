0da43b83d67f6a4aa3e5e2b70d42a5910984c9bc
keiank <278810033+keiank@users.noreply.github.com>
2026-06-06 20:58:40 -0400

    Add documentation to logger middleware

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,5 +1,12 @@
 import type { Request, Response, NextFunction } from 'express';
 
+/**
+ * Middleware that logs basic request information.
+ *
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @param {NextFunction} next - Express next middleware function.
+ */
 function logger(req: Request, res: Response, next: NextFunction) {
   console.log(`${req.method} - ${req.path}`);
   next();
