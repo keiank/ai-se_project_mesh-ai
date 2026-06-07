0b473e7cdc26fc5c82f9ac06944c62ec47f76d79
keiank <278810033+keiank@users.noreply.github.com>
2026-06-06 21:12:02 -0400

    Set up server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/index.js';
 import { logger } from './middleware/logger.js';
+import { errorHandler, notFoundHandler } from './middleware/error.js';
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 // middleware
 app.use(express.json());
 app.use(logger);
+
 // routes
 app.get('/health', (req, res) => {
   res.status(200).json({
@@ -16,8 +18,12 @@ app.get('/health', (req, res) => {
     error: null,
   });
 });
-
+app.get('/test-error', (req, res) => {
+  throw new Error('Test error');
+});
 app.use(router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
