218de99f9a3cd59f0d8222be5f4e710b202e88e6
Keian Kaserman <278810033+keiank@users.noreply.github.com>
2026-06-24 17:33:17 -0400

    Use environment variables to set application settings

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import router from './routes/index.js';
 import { logger } from './middleware/logger.js';
 import { errorHandler, notFoundHandler } from './middleware/error.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
